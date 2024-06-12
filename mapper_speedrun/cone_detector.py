46073856e51a97812255ed22fad46dc0cd91d039
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-06-12 19:45:58 +0100

    convert bounding box positions from inference coordinates to camera

--- mapper_speedrun/cone_detector.py
+++ mapper_speedrun/cone_detector.py
@@ -11,11 +11,35 @@ class ConeDetector:
         self.confidence_thres = confidence_thres
         self.iou_thres = iou_thres
         self.infer_size = infer_size
+        self.original_size = None
+        
+    def infer_pixel_to_original(self, bbox: bbox_t) -> bbox_t:
+        """
+        Convert the bbox from the infer size to the original size.
+        
+        Args:
+            bbox (bbox_t): The bbox to convert.
+        
+        Returns:
+            bbox_t: The converted bbox.
+        """
+        
+        # get the scale factor
+        scale_x = self.original_size[0] / self.infer_size
+        scale_y = self.original_size[1] / self.infer_size
+        
+        # convert the bbox
+        bbox = bbox_t(bbox.x * scale_x, bbox.y * scale_y, bbox.w * scale_x, bbox.h * scale_y, bbox.score, bbox.class_id)
+        
+        return bbox
 
     def predict(self, image) -> List[bbox_t]:
 
         # convert image from bgr to rgb
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # get the original size
+        self.original_size = (image.shape[1], image.shape[0])
 
         # resize image to 640x640
         image = cv2.resize(image, (self.infer_size, self.infer_size), interpolation=cv2.INTER_LINEAR)
@@ -34,5 +58,8 @@ class ConeDetector:
 
         # filter with nms-iou
         bboxes = nms_iou(boxes, probs, boxes.shape[1], probs.shape[2], self.iou_thres, self.confidence_thres)
+
+        # convert the bboxes to the original size
+        bboxes = [self.infer_pixel_to_original(bbox) for bbox in bboxes]
 
         return bboxes
