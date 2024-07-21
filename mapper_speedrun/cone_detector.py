211d66754f77a6174ecdf8aa869f29be62c206ee
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-07-21 20:22:01 +0100

    perform inference on gpu

--- mapper_speedrun/cone_detector.py
+++ mapper_speedrun/cone_detector.py
@@ -1,3 +1,4 @@
+import onnxruntime as ort
 from onnxruntime import InferenceSession
 import numpy as np
 import cv2
@@ -7,12 +8,35 @@ from .types import bbox_t
 from .filtering import nms_iou
 
 class ConeDetector:
-    def __init__(self, model_path: str, confidence_thres: float = 0.5, iou_thres: float = 0.4, infer_size: int = 640):
-        self.model: InferenceSession = InferenceSession(model_path)
+    def __init__(self, model_path: str, confidence_thres: float = 0.65, iou_thres: float = 0.4, infer_size: int = 640):
         self.confidence_thres = confidence_thres
         self.iou_thres = iou_thres
         self.infer_size = infer_size
         self.original_size = None
+
+        """
+        self.providers = [
+            ('CUDAExecutionProvider', {
+                'device_id': 0,
+                'arena_extend_strategy': 'kNextPowerOfTwo',
+                'gpu_mem_limit': 2 * 1024 * 1024 * 1024,
+                'cudnn_conv_algo_search': 'EXHAUSTIVE',
+                'do_copy_in_default_stream': True,
+            }),
+            'CPUExecutionProvider',
+        ]
+        """
+        ort.set_default_logger_severity(3)
+        self.providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] # TODO: in Jetson, add TensorrtExecutionProvider
+        self.model: InferenceSession = InferenceSession(model_path, providers=self.providers)
+        print(f"Model running on {self.model.get_providers()}")
+
+        # do some warmup
+        print("Warming up inference...", end="")
+        warmup_image = np.zeros((self.infer_size, self.infer_size, 3), dtype=np.uint8)
+        for i in range(10):
+            self.predict(warmup_image)
+        print("done")
         
     def infer_pixel_to_original(self, bbox: bbox_t) -> bbox_t:
         """
@@ -50,13 +74,18 @@ class ConeDetector:
 
         # add the batch size channel
         img_tensor = img_tensor[None]
+        img_tensor = img_tensor.astype(np.float32)
 
         # run inference
-        inference_start = time.time()
-        output = self.model.run(None, {self.model.get_inputs()[0].name: img_tensor.astype(np.float32)})
-        inference_end = time.time()
-        # print(f"Inference time: {inference_end-inference_start}s")
+        try:
+            inference_start = time.time()
+            output = self.model.run(None, {'images': img_tensor})
+            inference_end = time.time()
+            print(f"Inference time: {inference_end - inference_start}s")
+        except Exception as e:
+            print(f"Inference failed: {e}")
 
+        # get the boxes and probs
         probs = output[0]
         boxes = output[1]
 
@@ -64,7 +93,7 @@ class ConeDetector:
         nms_start = time.time()
         bboxes = nms_iou(boxes, probs, boxes.shape[1], probs.shape[2], self.iou_thres, self.confidence_thres)
         nms_end = time.time()
-        # print(f"NMS time: {nms_end-nms_start}s")
+        print(f"NMS time: {nms_end-nms_start}s")
 
         # convert the bboxes to the original size and remove the ones outside
         bboxes = [self.infer_pixel_to_original(bbox) for bbox in bboxes if bbox.x + (bbox.w / 2) >= 0 and bbox.y + (bbox.h / 2) >= 0 and bbox.x + (bbox.w / 2) <= self.infer_size and bbox.y + (bbox.h / 2) <= self.infer_size]
